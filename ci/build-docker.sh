f092fc188e394583e37ce5cf525df540ee6d9675
Jens Schütt <Jesch20@student.sdu.dk>
2022-08-17 20:14:49 +0000

    dockerhub

---
+++ ci/build-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
+docker build -t "${DockerRepo}backend:latest" -t "${DockerRepo}backend-$Tag" backend/
+docker build -t "${DockerRepo}frontend:latest" -t "${DockerRepo}frontend-$Tag" frontend/
+
