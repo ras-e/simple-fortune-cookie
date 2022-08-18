035c907386eab84e86a37dce4c965f0fb4b15b47
Victor <viboy20@student.sdu.dk>
2022-08-18 10:54:47 +0000

    Added curl tests to pipeline

--- curl-test.sh
+++ curl-test.sh
@@ -1,2 +1,5 @@
 curl localhost:8080
+curl -X POST -H "Content-Type: application/json" \
+    -d '{"message": "Thats how the cookie crumbles"}' \
+    localhost:8080/api/add
 curl localhost:8080/api/all
