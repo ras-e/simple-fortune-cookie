9ec8e9c560a2b23b69315a92d4be5d61dd4072b6
Victor <viboy20@student.sdu.dk>
2022-08-19 13:14:44 +0000

    Update docker push scripts to include git commit in tag

--- ci/push-docker-dev.sh
+++ ci/push-docker-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-docker push "$docker_username/backend-$Tag-${GIT_COMMIT::8}" 
+docker push "$docker_username/backend-$Tag" 
 docker push "$docker_username/backend:latest-dev" 
-docker push "$docker_username/frontend-$Tag-${GIT_COMMIT::8}" 
+docker push "$docker_username/frontend-$Tag" 
 docker push "$docker_username/frontend:latest-dev" &
 wait
